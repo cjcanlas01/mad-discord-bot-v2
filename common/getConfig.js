dece89a15e30a766da674dcf6d276ecb333cbb75
ChristianJohnGSS <christianjohn.gss@gmail.com>
2020-06-11 22:02:29 +0800

    Change variable naming for bot token

--- common/getConfig.js
+++ common/getConfig.js
@@ -4,7 +4,7 @@ module.exports = function getConfig() {
     return {
         PREFIX1: process.env.PREFIX1,
         PREFIX2: process.env.PREFIX2,
-        TOKEN: process.env.TOKEN,
+        TOKEN: process.env.BOT_TOKEN,
         SPREADSHEET_ID: process.env.SPREADSHEET_ID,
         GOOGLE_SERVICE_ACCOUNT_EMAIL: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
         GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
