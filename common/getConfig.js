6239463485dc76dd3673c2e6d390a737d0ab288f
christianjohn.gss <christianjohn.gss@gmail.com>
2020-06-27 16:45:43 +0800

    Remove second prefix

--- common/getConfig.js
+++ common/getConfig.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 module.exports = function getConfig() {
     return {
         PREFIX1: process.env.PREFIX1,
-        PREFIX2: process.env.PREFIX2,
         TOKEN: process.env.BOT_TOKEN,
         SPREADSHEET_ID: process.env.SPREADSHEET_ID,
         GOOGLE_SERVICE_ACCOUNT_EMAIL: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
