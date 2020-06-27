8a11f5870a8f1a436744d1afa0b771f658fe7588
christianjohn.gss <christianjohn.gss@gmail.com>
2020-06-27 16:26:45 +0800

    Clean queue when stop-po is called

--- commands/stop-po.js
+++ commands/stop-po.js
@@ -1,5 +1,5 @@
 const { addRowData, prepTrackData, addOrRemoveRole, getCurrentPO } = require('../common/trackingSystem');
-const { hasPoAccess } = require('../common/utilities');
+const { hasPoAccess, writeJson, displayQueue } = require('../common/utilities');
 const { msgPoHasNoAccess } = require('../common/messages');
 const config = require('../common/getConfig')();
 
@@ -34,6 +34,43 @@ module.exports = {
 
 				message.channel.send('The Protocole officer is leaving, the buffs will not be available until another one take the role. Thank you !');
 				message.channel.send('https://static.wixstatic.com/media/3bd738_28c4b141811146a9b8d86c05d224b079~mv2.jpg/v1/fill/w_569,h_427,al_c,q_80/madclosed_copy.webp');
+
+				// Clean queue
+				const queueObj = [
+					{
+						"name": "Chief Builder",
+						"value": "[EMPTY]",
+						"inline": true
+					},
+					{
+						"name": "Grand Maester",
+						"value": "[EMPTY]",
+						"inline": true
+					},
+					{
+						"name": "Master of Ships",
+						"value": "[EMPTY]",
+						"inline": true
+					},
+					{
+						"name": "Master of Whisperers",
+						"value": "[EMPTY]",
+						"inline": true
+					},
+					{
+						"name": "Lord Commander",
+						"value": "[EMPTY]",
+						"inline": true
+					}
+				];
+				
+				writeJson('/data/queue.json', queueObj)
+				.then((data) => {
+					displayQueue(message);
+				})
+				.catch((err) => {
+				    throw new Error(err);
+				});
 				return;
 			}
 		} else {
