882fae15a9eab15eb72eadb8f6be4eab9f5e04f9
Psykoral <psykoral@gmail.com>
2021-02-25 22:30:28 -0800

    Returning list of alts when no arguments set

--- commands/alts.js
+++ commands/alts.js
@@ -11,47 +11,41 @@ module.exports = {
      * If record is not found, create record
      * else update record
      */
-    const [data, exists] = await m.Alts.select(
-        {
+    const data = await m.Alts.findAll({
+      attributes: ['alts'],
+        where: {
           playerId: message.author.id,
-        },
-        {
-          returning: true,
         }
-    );
-    // const [, isCreated] = await m.Alts.upsert(
-    //   {
-    //     playerId: message.author.id,
-    //     alts: args.join(" "),
-    //   },
-    //   {
-    //     returning: true,
-    //   }
-    // );
-  
-    console.log(exists);
-    console.log(data);
-  
-    // if (!isCreated && isArrayEmpty(args)) {
-    //   message.channel.send("Seems I can't find your alts, did you put it in?");
-    //   return;
-    // }
+    });
 
-    // if (isCreated) {
-    //   message.channel.send(
-    //     `${message.author.toString()}, your alt record has been added.`
-    //   );
-    // } else if (isCreated && isArrayEmpty(args)) {
-    //   /**
-    //    * Show them their current !alts list
-    //    */
-    //   message.channel.send(
-    //       `${message.author.toString()}, your current alt record is: ${data.dataValues.alts}`
-    //   );
-    // } else {
-    //   message.channel.send(
-    //     `${message.author.toString()}, your alt record has been updated.`
-    //   );
-    // }
+    const alts = (!isArrayEmpty(data)) ? data[0].dataValues.alts : false;
+
+    if (!!alts && isArrayEmpty(args)) {
+      return message.channel.send(`${message.author.toString()}, your current alt record is: ${alts}`);
+    }
+
+    const [, isCreated] = await m.Alts.upsert(
+      {
+        playerId: message.author.id,
+        alts: args.join(" "),
+      },
+      {
+        returning: true,
+      }
+    );
+
+    if (!alts && !isCreated && isArrayEmpty(args)) {
+      return message.channel.send("Seems I can't find your alts, did you put it in?");
+    }
+
+    if (isCreated) {
+      message.channel.send(
+        `${message.author.toString()}, your alt record has been added.`
+      );
+    } else {
+      message.channel.send(
+        `${message.author.toString()}, your alt record has been updated.`
+      );
+    }
   },
 };
