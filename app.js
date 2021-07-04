a9af598bad033d4da9309b6c363b34d2d1954f90
cjcanlas01 <cjcanlas78@gmail.com>
2021-07-05 00:39:38 +0800

    chore: prepare code for HOH

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const Discord = require("discord.js");
 const config = require("./common/getConfig")();
 const client = new Discord.Client();
-const bootstrapWelcome = require("./bootstrap/welcome");
 const bootstrapCommand = require("./bootstrap/command");
 const db = require("./common/db");
 
@@ -26,9 +25,6 @@ const db = require("./common/db");
  */
 client.commands = new Discord.Collection();
 client.roleCommands = new Discord.Collection();
-client.configs = new Discord.Collection();
-client.banks = new Discord.Collection();
-client.welcomeMsgs = new Discord.Collection();
 
 // Get all command file names
 const commandFiles = fs
@@ -42,21 +38,12 @@ const commandFiles = fs
  */
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
-  if (command.isRoleCommand) {
-    client.roleCommands.set(command.name, command);
-  } else {
-    client.commands.set(command.name, command);
-  }
+  client.commands.set(command.name, command);
 }
 
 client.once("ready", async () => {
   console.log("Ready!");
   client.user.setActivity(`${config.PREFIX}madhelp`, { type: "LISTENING" });
-  setConfigVariables();
-  bootstrapWelcome(client);
-  // setInterval(() => {
-  //   bootstrapBubble(client);
-  // }, 60000);
 });
 
 client.on("message", (message) => {
@@ -65,36 +52,3 @@ client.on("message", (message) => {
 
 client.login(config.TOKEN);
 
-const setConfigVariables = async () => {
-  const m = require("./models/index");
-  const getConfigList = await m.Config.findAll({
-    attributes: ["config", "value"],
-  });
-  const getBankConfig = await m.Bank.findAll({
-    attributes: ["name", "transport_tax", "transport_amount"],
-  });
-  const getMsgList = await m.WelcomeMessage.findAll({
-    attributes: ["guild", "channel", "message"],
-  });
-
-  const configList = getConfigList.map((c) => c.dataValues);
-  const bankList = getBankConfig.map((c) => c.dataValues);
-  const msgList = getMsgList.map((c) => c.dataValues);
-  for (const config of configList) {
-    client.configs.set(config.config, config.value);
-  }
-
-  for (const bank of bankList) {
-    client.banks.set(bank.name, {
-      TRANSPORT_TAX: bank.transport_tax,
-      TRANSPORT_AMOUNT: bank.transport_amount,
-    });
-  }
-
-  for (const msg of msgList) {
-    client.welcomeMsgs.set(msg.guild, {
-      WELCOME_CHANNEL: msg.channel,
-      MESSAGE: msg.message,
-    });
-  }
-};
--- common/getConfig.js
+++ common/getConfig.js
@@ -4,9 +4,5 @@ module.exports = function getConfig() {
   return {
     PREFIX: process.env.PREFIX || "!",
     TOKEN: process.env.BOT_TOKEN,
-    SPREADSHEET_ID: process.env.SPREADSHEET_ID,
-    GOOGLE_SERVICE_ACCOUNT_EMAIL: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    TIME_STORE_SPREADSHEET_ID: process.env.TIME_STORE_SPREADSHEET_ID,
   };
 };
