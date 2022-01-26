f7585973f3848281ed0b9ed78b0d32a198375e2e
cjcanlas01 <cjcanlas78@gmail.com>
2022-01-26 10:39:29 +0800

    update

--- common/db.js
+++ common/db.js
@@ -1,4 +1,4 @@
 const { Sequelize } = require("sequelize");
-const config = require("./common/getConfig")();
+const config = require("../common/getConfig")();
 
 module.exports = new Sequelize(config.CLEARDB_DATABASE_URL);
