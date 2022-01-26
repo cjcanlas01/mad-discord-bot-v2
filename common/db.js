fca1ba58925d9cdf826e143fafaef3050e2da65a
cjcanlas01 <cjcanlas78@gmail.com>
2022-01-26 09:18:23 +0800

    update db connection

--- common/db.js
+++ common/db.js
@@ -2,9 +2,17 @@ const env = process.env.NODE_ENV || "development";
 const config = require("../config/config.json")[env];
 const { Sequelize } = require("sequelize");
 
-module.exports = new Sequelize(
-  config.database,
-  config.user,
-  config.password,
-  config
-);
+let sequelize;
+
+if (config && config.use_env_variable) {
+  sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL, config);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.user,
+    config.password,
+    config
+  );
+}
+
+module.exports = sequelize;
